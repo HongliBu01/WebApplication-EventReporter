2f54b55a5b3dc18741b9908dade0ee5949ac31e1
Hongli Bu <honglibu@cs.uchicago.edu>
2018-01-20 02:45:33 -0600

    version0.5

---
+++ around-web/src/components/Header.js
@@ -0,0 +1,26 @@
+import React  from 'react';
+import logo from '../assets/images/logo.svg';
+import { Icon } from 'antd';
+import PropTypes from 'prop-types';
+export class Header extends React.Component {
+    static propTypes = {
+        isLoggedIn: PropTypes.bool.isRequired,
+        handleLogout: PropTypes.func.isRequired,
+    }
+    render() {
+        return (
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <h1 className="App-title">DiscoverNearby</h1>
+                {
+                    this.props.isLoggedIn ?
+                        <a className="logout"
+                           onClick={this.props.handleLogout}
+                        >
+                            <Icon type="logout" />{' '}Logout
+                        </a> : null
+                }
+            </header>
+        );
+    }
+}
